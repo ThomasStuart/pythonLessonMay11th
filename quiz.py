97b5fcb11248e74b16fc497e3447d6a4977652bd
Thoms Stuart <31195539+ThomasStuart@users.noreply.github.com>
2020-05-11 17:19:25 -0700

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,35 @@
+# Question 1: Programming question
+# step 1 make an empty list called nums
+# step 2 using a for loop add the values 1-10
+
+
+# Question 2: For-loops used strictly for lists.  True/False
+
+
+# Question 3:  use a for loop to print the following
+# 1, 9
+# 9, 3
+# 3, 2
+# 2, 6
+pairs = [1,9,3,2,6]
+
+
+# Question 4:  fill in code that returns true/false
+
+def doesHaveNumGreaterThan100(list):
+    # fill in code
+    return
+
+list1 = [12,44,89,97] 
+list2 = [1,13,181,77] 
+
+doesHaveNumGreaterThan100(list1)  # returns False 
+doesHaveNumGreaterThan100(list2)  # returns True 
+
+# Question 5: print the following pattern using a for loop
+# hint: use a for loop within a for loop  (nested for loop)
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
